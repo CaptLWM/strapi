8127e46708c06492d694a4a7d313630a176bb347
CaptLWM <wonmin275@gmail.com>
2023-01-06 21:09:44 +0900

    .

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '36921f7028089a922ae45691f61df18f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ae5c827a-a355-4cbd-836f-ff6d2528407d'
+};
